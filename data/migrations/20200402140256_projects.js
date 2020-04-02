56065218e6a88c2548bb04dc458ee1a077c7c799
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-04-02 14:36:41 -0400

    adds migration structure for projects

--- data/migrations/20200402140256_projects.js
+++ data/migrations/20200402140256_projects.js
@@ -2,7 +2,19 @@ const cleaner = require("knex-cleaner");
 exports.up = function(knex) {
   return knex.schema.createTable("projects", tbl => {
     tbl.increments();
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+    tbl.string("start_data").notNullable();
+    tbl.string("end_date").notNullable();
+    tbl.string("image");
+    tbl.string("description", 1000).notNullable();
+    tbl.string("role", 128).notNullable();
+    tbl.string("stack", 512).notNullable();
   });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("projects");
+};
