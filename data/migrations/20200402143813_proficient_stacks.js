30c1312c21b813de0975809065e3350083edfa2b
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-04-02 14:40:56 -0400

    adds finished migration file for proficient stacks

--- data/migrations/20200402143813_proficient_stacks.js
+++ data/migrations/20200402143813_proficient_stacks.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("proficient_stacks", tbl => {
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .nonNullable()
+      .unique();
+    tbl.string("specifics");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("proficient_stacks");
 };
